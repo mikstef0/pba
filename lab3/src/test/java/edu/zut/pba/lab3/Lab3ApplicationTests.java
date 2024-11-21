e9bdc251448955662a847ed56853723eb0dfb6c9
Mikołaj Stefaniszyn <111416746+mikstef0@users.noreply.github.com>
2024-11-21 20:58:05 +0100

    lab3
    lab3 v1

---
+++ lab3/src/test/java/edu/zut/pba/lab3/Lab3ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.zut.pba.lab3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
